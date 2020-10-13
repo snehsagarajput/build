c328a69a3661a844f6119ae88fcf829245d2a28e
Sneh Sagar Rajput <sneh.sagar98@gmail.com>
2020-10-13 11:40:11 +0530

    Final

---
+++ build/precache-manifest.e1b212b06bb6f7c2412e8fbeb6bea4a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50aa8e10de8f78d7e60022094ffd8e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4123b773c796270a8cfb",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "be962bc10ceaa9a49661",
+    "url": "/static/css/main.46900260.chunk.css"
+  },
+  {
+    "revision": "4123b773c796270a8cfb",
+    "url": "/static/js/2.ab29c2a9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ab29c2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be962bc10ceaa9a49661",
+    "url": "/static/js/main.60c82e7e.chunk.js"
+  },
+  {
+    "revision": "448ace6e5f844cacaa7b",
+    "url": "/static/js/runtime-main.0b87e349.js"
+  },
+  {
+    "revision": "b218bc6dd411dec5af1b9c19bbb81dd7",
+    "url": "/static/media/content.b218bc6d.jpg"
+  },
+  {
+    "revision": "66500d9cd5c3a22a6bd21a1c809e305d",
+    "url": "/static/media/style.66500d9c.jpg"
+  },
+  {
+    "revision": "bf8b6da38328148dcb06333b92ffb89e",
+    "url": "/static/media/styled.bf8b6da3.png"
+  }
+]);
